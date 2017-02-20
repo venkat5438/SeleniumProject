cc94c3ea6c50042d95fd7b670d9bd374a867761a
venkat5438 <venkatramreddy108@gmail.com>
2017-02-20 15:29:51 +0530

    package name modified

--- src/com/selenium/testng/BaseClass.java
+++ src/com/selenium/testng/BaseClass.java
@@ -1,4 +1,4 @@
-package com.selenium.packge;
+package com.selenium.testng;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
--- src/com/selenium/testng/LoginScripts.java
+++ src/com/selenium/testng/LoginScripts.java
@@ -1,4 +1,4 @@
-package com.selenium.packge;
+package com.selenium.testng;
 
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
